d6285764dcfe151ab48bad70722e4f8556268dfe
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-04 10:50:16 +0700

    Seperate `theme.js`

---
+++ src/styles/theme/index.js
@@ -0,0 +1,12 @@
+import breakpoints from "./breakpoints";
+import colors from "./colors";
+import effects from "./effects";
+
+
+const theme = {
+	breakpoints,
+	colors,
+	effects,
+};
+
+export default theme;
