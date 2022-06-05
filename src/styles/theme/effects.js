f912c1f15343f6ca4b11d477203692a40cd3dc0f
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-05 20:32:35 +0700

    Create `hoverClick` effect

--- src/styles/theme/effects.js
+++ src/styles/theme/effects.js
@@ -1,8 +1,25 @@
+import { css } from "styled-components";
+import baseTransitionMs from "../global/base-transition-ms";
+
 const effects = {
-	1 : "box-shadow: -1px 6px 20px rgba(194, 199, 225, 0.25), -10px 24px 35px rgba(183, 208, 249, 0.44);",
-	2 : "box-shadow: 20px 10px 20px rgba(100, 141, 229, 0.25);",
-	3 : "box-shadow: 0px 0px 20px rgba(194, 199, 225, 0.25), -5px 14px 35px rgba(183, 208, 249, 0.44);",
-	4 : "box-shadow: 2px 0px 20px rgba(100, 141, 229, 0.25), 6px 0px 20px rgba(183, 208, 249, 0.44);",
+	boxShadows : {
+		1 : "box-shadow: -1px 6px 20px rgba(194, 199, 225, 0.25), -10px 24px 35px rgba(183, 208, 249, 0.44);",
+		2 : "box-shadow: 20px 10px 20px rgba(100, 141, 229, 0.25);",
+		3 : "box-shadow: 0px 0px 20px rgba(194, 199, 225, 0.25), -5px 14px 35px rgba(183, 208, 249, 0.44);",
+		4 : "box-shadow: 2px 0px 20px rgba(100, 141, 229, 0.25), 6px 0px 20px rgba(183, 208, 249, 0.44);",
+	},
+	hoverClick : css`
+		transition: filter ${baseTransitionMs * 0.4}ms ease-in-out;
+
+		&:hover {
+			cursor: pointer;
+			filter: brightness(105%);
+		}
+
+		&:active {
+			filter: brightness(95%);
+		}
+	`,
 };
 
 export default effects;
