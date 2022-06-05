b96eea636fe6b50b48565f297e16aa0dcf655bfc
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-05 23:19:35 +0700

    Create `GradientRect`

---
+++ src/components/Shapes/GradientRect.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const GradientRect = styled.div`
+    background: linear-gradient(180deg, hsla(0, 0%, 100%, 0.3) 0%, rgba(196, 196, 196, 0) 100%);
+    border-radius: 1em 1em 0 0;
+    width: 7.3em;
+    height: 7.3em;
+`;
+
+export default GradientRect;
