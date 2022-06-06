d8bf164884f22b094a2ca9fd2a746c6ffba02681
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-06 09:57:31 +0700

    Add `effects.borderRadius`

--- src/styles/theme/effects.js
+++ src/styles/theme/effects.js
@@ -2,7 +2,8 @@ import { css } from "styled-components";
 import baseTransitionMs from "../global/base-transition-ms";
 
 const effects = {
-	boxShadows : {
+	borderRadius : "border-radius: 10px;",
+	boxShadows   : {
 		1 : "box-shadow: -1px 6px 20px rgba(194, 199, 225, 0.25), -10px 24px 35px rgba(183, 208, 249, 0.44);",
 		2 : "box-shadow: 20px 10px 20px rgba(100, 141, 229, 0.25);",
 		3 : "box-shadow: 0px 0px 20px rgba(194, 199, 225, 0.25), -5px 14px 35px rgba(183, 208, 249, 0.44);",
