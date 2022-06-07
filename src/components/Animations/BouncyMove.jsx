3a64137250212c41e66462844b67522beb50178b
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-07 10:48:43 +0700

    Refactor `BouncyMove` to be a smart component

--- src/components/Animations/BouncyMove.jsx
+++ src/components/Animations/BouncyMove.jsx
@@ -1,22 +1,36 @@
+import React, { useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import baseTransitionMs from "../../styles/global/base-transition-ms";
 
 /**
  * Activate `bouncyMove[Left | Right]` animation on enter and exit.
  *
- * @param {{children: any, transitionIn: any, direction?: "left" | "right", CSSTransitionOpts?: any}} props
+ * @param {{children: any, onClick: (e: Event) => void, direction?: "left" | "right", CSSTransitionOpts?: any}} props
  *
  * @returns
  */
-const BouncyMove = ({ children, transitionIn, direction = "left", CSSTransitionOpts = {} }) => {
+const BouncyMove = ({
+	children,
+	onClick,
+	direction = "left",
+	CSSTransitionOpts = {}
+}) => {
+	const [ isClicked, setIsClicked ] = useState(false);
+
+	const onClickHandler = e => {
+		setIsClicked(true);
+		onClick(e);
+	};
+
 	return (
 		<CSSTransition
 			classNames={`bouncy-move-${direction}`}
-			in={transitionIn}
+			in={isClicked}
 			timeout={baseTransitionMs}
 			{...CSSTransitionOpts}
+			onEntered={() => setIsClicked(false)}
 		>
-			{children}
+			{React.cloneElement(children, { onClick : onClickHandler })}
 		</CSSTransition>
 	);
 };
