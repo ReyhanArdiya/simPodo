8f3fa64fe293b728b27b20a21c1a182c50773f06
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-07 10:58:33 +0700

    Seperate `Arrow` into 3 components
    This is to allow animations like `BouncyMove` to work without overwriting the arrow's rotation

--- src/components/Shapes/Arrows/ArrowDown.jsx
+++ src/components/Shapes/Arrows/ArrowDown.jsx
@@ -10,19 +10,6 @@ const Container = styled.svg.attrs({
     stroke: ${({ dark, theme }) => dark ? theme.colors.dark.UI[5] : theme.colors.light.UI[6]};
 
     ${({ theme }) => theme.effects.hoverClick}
-
-    transform: rotate(${({ angle }) => {
-		switch (angle) {
-			case "left" :
-				return "90";
-			case "right" :
-				return "-90";
-			case "up" :
-				return "180";
-			default :
-				return "0";
-		}
-	}}deg);
 `;
 
 const Path = styled.path.attrs({ d : "M1.5 1.66998L6.5 6.66998L11.5 1.66998", })`
@@ -30,10 +17,9 @@ const Path = styled.path.attrs({ d : "M1.5 1.66998L6.5 6.66998L11.5 1.66998", })
 	stroke-width: 0.3em;
 `;
 
-const Arrow = ({ onClick, angle = "down", className = "", dark = false }) => {
+const ArrowDown = ({ onClick, className = "", dark = false }) => {
 	return (
 		<Container
-			angle={angle}
 			className={className}
 			dark={dark}
 		>
@@ -44,4 +30,4 @@ const Arrow = ({ onClick, angle = "down", className = "", dark = false }) => {
 	);
 };
 
-export default Arrow;
+export default ArrowDown;
