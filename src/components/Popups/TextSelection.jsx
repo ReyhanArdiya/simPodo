b2fddc11c3e0f46cb7204f77c933db4962dc1bda
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-08 14:42:56 +0700

    Create `TextSelection` and `TextSelectionSm`

---
+++ src/components/Popups/TextSelection.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import Card from "../Cards/Card";
+
+const TextSelection = styled(Card).attrs({ as : "li" })`
+    list-style: none;
+    font: 900 100em "Inter", sans-serif;
+    text-align: center;
+    padding: 0.5em 1.5em;
+    border-radius: 0.3em;
+    z-index: 2;
+    cursor: pointer;
+
+    transition: color 0.1s ease-in-out;
+
+    color: ${({ dark, theme }) => dark ? theme.colors.dark.UI[5] : theme.colors.light.UI[2]};
+
+    ${({ active }) => active ? "&" : ":hover"} {
+        color: ${({ dark, theme }) => dark ? theme.colors.dark.UI[2] : theme.colors.light.UI[1]};
+        background-image: url("/images/bg-text-selection-sm.png");
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 100% 100%;
+        background-color: ${({ backgroundColor }) => backgroundColor};
+    }
+`;
+
+export default TextSelection;
+
---
+++ src/components/Popups/TextSelectionSm.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import TextSelection from "./TextSelection";
+
+const Container = styled(TextSelection)`
+	font-size: 1.6em;
+`;
+
+const TextSelectionSm = ({
+	children: text,
+	backgroundColor,
+	active = false,
+	dark = false
+}) => {
+	return (
+		<Container
+			active={active}
+			backgroundColor={backgroundColor}
+			dark={dark}
+		>
+			{text}
+		</Container>
+	);
+};
+
+export default TextSelectionSm;
