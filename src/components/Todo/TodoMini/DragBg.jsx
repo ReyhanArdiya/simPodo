74b4ba573cd7efb9f7cc70817c4377f74a67fe7c
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-10 15:23:30 +0700

    Create `DragBg`

---
+++ src/components/Todo/TodoMini/DragBg.jsx
@@ -0,0 +1,51 @@
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    border-radius: inherit;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Indicator = styled.div`
+    flex-grow: 0.15;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    border-radius: inherit;
+    color: ${({ dark, theme }) => dark ? theme.colors.dark.UI[1] : theme.colors.light.UI[1]};
+    font-size: 2em;
+`;
+
+const Delete = styled(Indicator)`
+    background-color: ${({ dark, theme }) => dark ? theme.colors.dark.tags[1] : theme.colors.light.tags[1]};
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+`;
+
+const Done = styled(Indicator)`
+    background-color: ${({ dark, theme }) => dark ? theme.colors.dark.tags[4] : theme.colors.light.tags[4]};
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+`;
+
+const DragBg = ({ dark = false }) => {
+	return (
+		<Container>
+			<Delete dark={dark}>
+				<FontAwesomeIcon icon={faTimes}/>
+			</Delete>
+			<Done dark={dark}>
+				<FontAwesomeIcon icon={faCheck}/>
+			</Done>
+		</Container>
+	);
+};
+
+export default DragBg;
