0fe8f107fabc041d9eda19f49e88e4cc1e1e2ff4
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-15 21:13:11 +0700

    Implement `themeSlice`

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
+import themeSlice, { themeSliceName } from "./theme/slice";
 
-const makeStore = () => configureStore({ reducer : {} });
+const makeStore = () => configureStore({ reducer : { [themeSliceName] : themeSlice.reducer } });
 
 const wrapper = createWrapper(makeStore, { debug : false });
 
