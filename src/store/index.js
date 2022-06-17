6232c98e385487f05e79cede9a44649e745a5a18
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-17 09:54:47 +0700

    Add all slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
+import authSlice, { authSliceName } from "./auth/slice";
+import calendarSlice, { calendarSliceName } from "./calendar/slice";
+import tagsSlice, { tagsSliceName } from "./tags/slice";
 import themeSlice, { themeSliceName } from "./theme/slice";
 import todosSlice, { todosSliceName } from "./todos/slice";
 
 const makeStore = () => configureStore({
 	reducer : {
-		[themeSliceName] : themeSlice.reducer,
-		[todosSliceName] : todosSlice.reducer
+		[authSliceName]     : authSlice.reducer,
+		[calendarSliceName] : calendarSlice.reducer,
+		[tagsSliceName]     : tagsSlice.reducer,
+		[themeSliceName]    : themeSlice.reducer,
+		[todosSliceName]    : todosSlice.reducer,
 	}
 });
 
