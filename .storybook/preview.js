e64dcbacd798ef9ca376561be6a7f1f6b077de42
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-17 20:25:02 +0700

    Seperate `GlobalStyle` to be reused in `storybook`

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import theme from "../src/styles/theme";
+import GlobalStyle from "../src/styles/global";
 
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
@@ -14,6 +15,7 @@ export const parameters = {
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Story />
     </ThemeProvider>
   ),
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,13 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import Head from "next/head";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import wrapper from "../store";
-import animations from "../styles/global/animations";
-import cssReset from "../styles/global/css-reset";
+import GlobalStyle from "../styles/global";
 import theme from "../styles/theme";
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-const GlobalStyle = createGlobalStyle`
-	${cssReset}
-	${animations}
-`;
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
