e4c7394f67318d9cd0eac407020a041d64ebe02d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-19 22:30:53 +0700

    Create `TodoBig` stories

--- src/components/Todo/TodoMini/Tag.jsx
+++ src/components/Todo/TodoMini/Tag.jsx
@@ -4,6 +4,7 @@ const Tag = styled.p`
     color: ${({ color }) => color};
     font: 300 1.2em "Nunito", sans-serif;
     letter-spacing: -0.03em;
+    ${({ edit }) => edit && "cursor: pointer;"}
 `;
 
 export default Tag;
