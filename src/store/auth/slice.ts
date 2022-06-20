1dac8c94a24f5b38c15af60555674f04409d320e
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-20 21:18:34 +0700

    Test `auth.slice`

--- src/store/auth/slice.ts
+++ src/store/auth/slice.ts
@@ -1,31 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice = createSlice({
-	initialState : {
-		user : {
-			localid  : "",
-			token    : "",
-			username : ""
-		}
-	},
-	name     : "auth",
-	reducers : {
-		changeUsername(state, { payload: username }) {
-			state.user.username = username;
-		},
-		login(state, { payload: { user:  { localid, token, username } } }) {
-			state.user.localid = localid;
-			state.user.token = token;
-			state.user.username = username;
-		},
-		logout(state) {
-			delete state.user;
-		},
-		refreshToken(state, { payload: token }) {
-			state.user.token = token;
-		},
+interface InitialState {
+	user?: {
+		localId: string;
+		token: string;
+		username: string;
 	}
-});
+}
+
+const initialState: InitialState = {
+	user : {
+		localId  : "",
+		token    : "",
+		username : ""
+	}
+};
+
+const authSlice = createSlice(
+	{
+		initialState,
+		name     : "auth",
+		reducers : {
+			changeUsername(state, { payload: username }) {
+				if (!state.user) {
+					throw new Error("User is not logged in");
+				}
+
+				state.user.username = username;
+			},
+			login(state, { payload: { user } }) {
+				state.user = user;
+			},
+			logout(state) {
+				delete state.user;
+			},
+			refreshToken(state, { payload: token }) {
+				if (!state.user) {
+					throw new Error("User is not logged in");
+				}
+
+				state.user.token = token;
+			},
+		}
+	}
+);
 
 export const { actions: authSliceActions, name: authSliceName } = authSlice;
 export default authSlice;
