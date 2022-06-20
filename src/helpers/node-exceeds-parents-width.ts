c9107642501d541f2077072dd1f5608a9bc5e008
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-20 21:48:34 +0700

    Type `nodeExceedsParentsWidth`

--- src/helpers/node-exceeds-parents-width.ts
+++ src/helpers/node-exceeds-parents-width.ts
@@ -1,13 +1,9 @@
-/**
- *
- * @param {number} leftPos
- *
- * @param {import("react").ReactNode} node
- *
- * @param {number} threshold
- */
-const nodeExceedsParentsWidth = (leftPos, node, threshold = 0.5) => {
-	return Math.abs(leftPos) >= node.parentElement.offsetWidth * threshold;
+const nodeExceedsParentsWidth = (
+	leftPos: number,
+	node: HTMLElement,
+	threshold = 0.5
+) => {
+	return Math.abs(leftPos) >= node.parentElement!.offsetWidth * threshold;
 };
 
 export default nodeExceedsParentsWidth;
