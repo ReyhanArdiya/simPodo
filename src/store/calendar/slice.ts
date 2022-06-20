16f038a47d247e87ca2011573769278b6bb44bba
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-20 20:39:28 +0700

    Type calendar slice

--- src/store/calendar/slice.ts
+++ src/store/calendar/slice.ts
@@ -4,39 +4,49 @@ const calendarSlice = createSlice({
 	initialState : {
 		selectedDate : {
 			date       : 0,
-			monthIndex : 0,
+			monthIndex : 0
 		},
 		viewDate : {
 			daysInMonth       : 31,
 			monthIndex        : 0,
-			startingDayOfWeek : 0,
+			startingDayOfWeek : 0
 		}
 	},
 	name     : "calendar",
 	reducers : {
-		selectDate : (state, { payload: newSelectedDate }) => {
+		selectDate : (
+			state,
+			{ payload: newSelectedDate }: { payload: number }
+		) => {
 			state.selectedDate.date = newSelectedDate;
 		},
-		selectMonth : (state, { payload: newSelectedMonth }) => {
+		selectMonth : (
+			state,
+			{ payload: newSelectedMonth }: { payload: number }
+		) => {
 			state.selectedDate.monthIndex = newSelectedMonth;
 		},
-		setDaysInMonth : (state, { payload: newDaysInMonth }) => {
+		setDaysInMonth : (
+			state,
+			{ payload: newDaysInMonth }: { payload: number }
+		) => {
 			state.viewDate.daysInMonth = newDaysInMonth;
 		},
-		setViewMonthIndex : (state, { payload: newMonthIndex }) => {
+		setViewMonthIndex : (
+			state,
+			{ payload: newMonthIndex } : { payload: number }
+		) => {
 			state.viewDate.monthIndex = newMonthIndex;
 		},
 		setViewStartingDayOfWeek : (
 			state,
-			{ payload: newStartingDayOfWeek }
+			{ payload: newStartingDayOfWeek }: { payload: number }
 		) => {
 			state.viewDate.startingDayOfWeek = newStartingDayOfWeek;
-		},
+		}
 	}
 });
 
-export const {
-	actions: calendarSliceActions,
-	name: calendarSliceName
-} = calendarSlice;
+export const { actions: calendarSliceActions, name: calendarSliceName } =
+	calendarSlice;
 export default calendarSlice;
