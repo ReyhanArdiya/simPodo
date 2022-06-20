c21a2bfebcc90020217f0150f7fcf08b5d97dd84
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-20 09:53:12 +0700

    Animate `SemanticInput` error

--- src/components/Forms/Inputs/SemanticInput.jsx
+++ src/components/Forms/Inputs/SemanticInput.jsx
@@ -1,17 +1,52 @@
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import Input from "./Input";
 import { v4 as uuidv4 } from "uuid";
+import { CSSTransition } from "react-transition-group";
+
+const shakeEm = 1;
+
+const shake = keyframes`
+	from {
+		transform: translateX(0);
+	}
+
+	20% {
+		transform: translateX(${shakeEm}em);
+	}
+
+	40% {
+		transform: translateX(-${shakeEm}em);
+	}
+
+	60% {
+		transform: translateX(${shakeEm}em);
+	}
+
+	80% {
+		transform: translateX(-${shakeEm}em);
+	}
+
+	to {
+		transform: translateX(0);
+	}
+`;
+
+const animationMs = 250;
 
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 0.5em;
+
+	&.has-error-enter-active {
+		animation: ${shake} ${animationMs}ms ease-in-out;
+	}
 `;
 
 const outlineWidth = 3;
 
 const BorderedInput = styled(Input)`
-	${({ colors, hasMsg, dark, theme, valid }) => {
+	${({ colors, hasError, dark, theme, valid }) => {
 		if (valid) {
 			return css`
 					outline: ${outlineWidth}px solid ${dark ? theme.colors.dark.semantic.good : theme.colors.light.semantic.good};
@@ -19,7 +54,7 @@ const BorderedInput = styled(Input)`
 			`;
 		}
 
-		if (hasMsg) {
+		if (hasError) {
 			return css`
 				outline: ${outlineWidth}px solid ${dark ? theme.colors.dark.semantic.bad : theme.colors.light.semantic.bad};
                 ${colors?.error && css`outline-color: ${colors.error};`}
@@ -61,19 +96,24 @@ const SemanticInput = props => {
 	const { valid, id = uuidv4() } = props;
 
 	return (
-		<Container
-			dark={props.dark}
-			valid={valid}
+		<CSSTransition
+			classNames="has-error"
+			in={!!props.errorMsg}
+			timeout={animationMs}
 		>
-			<BorderedInput
-				{...props}
-				colors={props.colors}
+			<Container
 				dark={props.dark}
-				hasMsg={props.errorMsg}
-				id={id}
 				valid={valid}
-			/>
-			{!valid && props.errorMsg &&
+			>
+				<BorderedInput
+					{...props}
+					colors={props.colors}
+					dark={props.dark}
+					hasError={props.errorMsg}
+					id={id}
+					valid={valid}
+				/>
+				{!valid && props.errorMsg &&
 				<ErrorMsg
 					color={props.colors?.error}
 					dark={props.dark}
@@ -81,8 +121,9 @@ const SemanticInput = props => {
 				>
 					{props.errorMsg}
 				</ErrorMsg>
-			}
-		</Container>
+				}
+			</Container>
+		</CSSTransition>
 	);
 };
 
