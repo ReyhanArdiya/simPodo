fbd320af3693552d1b4dbf0a0a269a9dad0ca8a7
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-23 16:26:54 +0700

    Type `ColorsPopup`

--- src/components/units/Popups/ColorsPopup.stories.tsx
+++ src/components/units/Popups/ColorsPopup.stories.tsx
@@ -1,14 +1,18 @@
 import { action } from "@storybook/addon-actions";
+import type { Meta, StoryFn } from "@storybook/react";
 import theme from "../../../styles/theme";
 import ColorsPopupComponent from "./ColorsPopup";
 
-/** @type {import("@storybook/react").Meta} */
-const Meta = {
+interface Args {
+	dark: boolean;
+}
+
+const meta: Meta<Args> = {
 	component : ColorsPopupComponent,
 	args      : { dark : false }
 };
 
-export const ColorsPopup = ({ dark, ...args }) => {
+export const ColorsPopup: StoryFn<Args> = ({ dark, ...args }) => {
 	const colors = Object.values(
 		theme.colors[dark ? "dark" : "light"].tags
 	).map(color => ({
@@ -16,11 +20,7 @@ export const ColorsPopup = ({ dark, ...args }) => {
 		onClick : action(`Clicked ${color}`)
 	}));
 
-	return <ColorsPopupComponent
-		{...args}
-		colors={colors}
-		dark={dark}
-	       />;
+	return <ColorsPopupComponent {...args} colors={colors} dark={dark} />;
 };
 
-export default Meta;
+export default meta;
