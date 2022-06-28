697f1e93c211fa1abd5f0d8b32a74aefec046125
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-28 20:38:49 +0700

    Rewrite todos for authSlice reducers tests

---
+++ src/store/auth/slice.reducers.test.ts
@@ -0,0 +1,10 @@
+describe("authSlice reducers for user's login state", () => {
+	it.todo("throws an error when logging in a logged in");
+	it.todo("logs the user in");
+
+	it.todo("throws an error when logging out a not logged in user");
+	it.todo("logs the user out");
+
+	it.todo("throws an error when refreshing a not logged in user's token");
+	it.todo("refreshes the token");
+});
