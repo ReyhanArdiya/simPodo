6ab1b5a3a861d773e5f328532474a369dcbbec88
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-28 21:27:43 +0700

    Refactor `authSlice` reducers

--- src/store/auth/slice.ts
+++ src/store/auth/slice.ts
@@ -1,38 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { IUser } from "../../models/user";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import UserAlreadyLoggedInError from "../../models/errors/user-already-logged-in-error";
+import UserNotLoggedInError from "../../models/errors/user-not-logged-in-error";
+import type User from "../../models/user";
 
-interface TodoSliceState {
-	user?: IUser;
+type StoreUser = Omit<User, "tags" | "todos">;
+
+export interface AuthSliceState {
+	user?: StoreUser;
 }
 
-const initialState: TodoSliceState = {};
+const initialState: AuthSliceState = {};
 
 const authSlice = createSlice(
 	{
 		initialState,
 		name     : "auth",
 		reducers : {
-			changeUsername(state, { payload: username }) {
+			changeUsername(state, { payload: username }: PayloadAction<User["username"]>) {
 				if (!state.user) {
-					throw new Error("User is not logged in");
+					throw new UserNotLoggedInError();
 				}
 
 				state.user.username = username;
 			},
-			login(state, { payload: { user } }) {
+			login(state, { payload: user }: PayloadAction<StoreUser>) {
+				if (state.user) {
+					throw new UserAlreadyLoggedInError();
+				}
+
 				state.user = user;
 			},
 			logout(state) {
+				if (!state.user) {
+					throw new UserNotLoggedInError();
+				}
+
 				delete state.user;
 			},
 			refreshToken(state, { payload: token }) {
 				if (!state.user) {
-					throw new Error("User is not logged in");
+					throw new UserNotLoggedInError();
 				}
 
 				state.user.token = token;
 			},
-		}
+		},
+
 	}
 );
 
