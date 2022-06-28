e7c749b04f7d3061190ba64ec5b478a7c32ac16c
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-28 08:51:14 +0700

    Fix jest for typescript

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir : "./", });
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const customJestConfig = {
 	moduleDirectories        : [ "node_modules", "<rootDir>/" ],
 	setupFilesAfterEnv       : [ "./jest.setup.js", "./src/tests/polyfill-textencoder.ts" ],
 	testEnvironment          : "jest-environment-jsdom",
 	modulePathIgnorePatterns : [ "node_modules", "jest-test-results.json" ],
+	preset                   : "ts-jest",
 };
 module.exports = createJestConfig(customJestConfig);
