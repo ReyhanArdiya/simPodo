798d16f1edb88f873f9617fd5b2b970990b4a20a
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-28 08:51:13 +0700

    Polyfill `TextEnconder` to test mongoose using jsdom

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir : "./", });
 const customJestConfig = {
 	moduleDirectories        : [ "node_modules", "<rootDir>/" ],
-	setupFilesAfterEnv       : [ "./jest.setup.js" ],
+	setupFilesAfterEnv       : [ "./jest.setup.js", "./src/tests/polyfill-textencoder.ts" ],
 	testEnvironment          : "jest-environment-jsdom",
 	modulePathIgnorePatterns : [ "node_modules", "jest-test-results.json" ],
 };
