a57a6b7fa68c32e6a67de4246a2a5ee71d925e9d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-06-29 16:53:35 +0700

    Rewrite calendar specs

--- src/store/calendar/slice.test.ts
+++ src/store/calendar/slice.test.ts
@@ -1,90 +1,31 @@
-import calendarSlice, { CalendarSliceState } from "./slice";
-
-const { actions, reducer } = calendarSlice;
-
-describe("Calendar slice", () => {
-	const expectedState = {
-		selectedDate : {
-			date       : expect.any(Number),
-			monthIndex : expect.any(Number),
-		},
-		viewDate : {
-			daysInMonth       : expect.any(Number),
-			monthIndex        : expect.any(Number),
-			startingDayOfWeek : expect.any(Number),
-		}
-	};
-
-	let initialState: CalendarSliceState;
-	beforeEach(() => {
-		initialState = {
-			selectedDate : {
-				date       : 0,
-				monthIndex : 0
-			},
-			viewDate : {
-				daysInMonth       : 31,
-				monthIndex        : 0,
-				startingDayOfWeek : 0
-			}
-		};
+describe("Calendar slice selected date field", () => {
+	describe("actions", () => {
+		it.todo("updates the selected date");
+		it.todo("updates the selected month");
+		it.todo("updates the selected year");
 	});
 
-	it("should contain these initialStates", () => {
-		expect(initialState).toEqual(expectedState);
+	describe("selectors", () => {
+		it.todo("selects the date");
+		it.todo("selects the month");
+		it.todo("selects the year");
+		it.todo("selects the full date in ISO 8601");
+	});
+});
+
+describe("Calendar slice viewed date field", () => {
+	describe("actions", () => {
+		it.todo("updates the viewed date");
+		it.todo("updates the viewed month");
+		it.todo("updates the viewed year");
 	});
 
-	describe("reducers", () => {
-		describe("selectedDate actions", () => {
-			it("should set the date", () => {
-				const newState = reducer(
-					initialState,
-					actions.dateSelected(2)
-				);
-
-				expect(newState.selectedDate.date)
-					.not.toEqual(initialState.selectedDate.date);
-			});
-
-			it("should set the monthIndex", () => {
-				const newState = reducer(
-					initialState,
-					actions.monthSelected(2)
-				);
-
-				expect(newState.selectedDate.monthIndex)
-					.not.toEqual(initialState.selectedDate.monthIndex);
-			});
-		});
-
-		describe("viewDate actions", () => {
-			it("should set the daysInMonth", () => {
-				const newState = reducer(
-					initialState,
-					actions.setDaysInMonth(2)
-				);
-
-				expect(newState.viewDate.daysInMonth)
-					.not.toEqual(initialState.viewDate.daysInMonth);
-			});
-			it("should set the monthIndex", () => {
-				const newState = reducer(
-					initialState,
-					actions.setViewMonthIndex(2)
-				);
-
-				expect(newState.viewDate.monthIndex)
-					.not.toEqual(initialState.viewDate.monthIndex);
-			});
-			it("should set the startingDayOfWeek", () => {
-				const newState = reducer(
-					initialState,
-					actions.setViewStartingDayOfWeek(2)
-				);
-
-				expect(newState.viewDate.startingDayOfWeek)
-					.not.toEqual(initialState.viewDate.startingDayOfWeek);
-			});
-		});
+	describe("selectors", () => {
+		it.todo("selects the date");
+		it.todo("selects the month");
+		it.todo("selects the year");
+		it.todo("selects the full date in ISO 8601");
+		it.todo("derives daysInMonth");
+		it.todo("derives firstDayOfMonth");
 	});
 });
