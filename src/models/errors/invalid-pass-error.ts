f63fe31567e763a9350ee22a1523e68e652852ac
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-10 11:42:33 +0700

    Improve `InvalidPassError` messages

--- src/models/errors/invalid-pass-error.ts
+++ src/models/errors/invalid-pass-error.ts
@@ -1,5 +1,32 @@
+export enum PassReqs {
+	MIN_LENGTH = 8,
+}
+
+export enum PassErrors {
+	TOO_SHORT,
+	NOT_ALPHANUMERIC,
+	NO_CAPITAL
+}
+
 export default class InvalidPassError extends Error {
-	constructor() {
-		super("Password is invalid!");
+	constructor(errorCode: PassErrors) {
+		let errMsg: string;
+
+		switch (errorCode) {
+			case PassErrors.TOO_SHORT :
+				errMsg = `Password must be at least ${PassReqs.MIN_LENGTH} characters`;
+				break;
+			case PassErrors.NOT_ALPHANUMERIC :
+				errMsg = "Password must contain numbers and letters";
+				break;
+			case PassErrors.NO_CAPITAL :
+				errMsg = "Password must contain one capital letter";
+				break;
+			default :
+				errMsg = "Password is invalid";
+				break;
+		}
+
+		super(errMsg);
 	}
 }
