75deee6f32084206fa51c0bc4f42daa2ceb137e4
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-10 10:46:27 +0700

    Fix `tagsSliceSelectors`

---
+++ src/store/tags/slice.selectors.test.ts
@@ -0,0 +1,23 @@
+import mockRootState from "../../tests/mock-rootstate";
+import { tagsSliceSelectors } from "./slice";
+
+const initialId = "1";
+const initialState = mockRootState;
+initialState.tags = {
+	tags : {
+		[initialId] : {
+			color : expect.any(String),
+			_id   : expect.any(String),
+			name  : expect.any(String)
+		}
+	}
+};
+
+describe("Tags slice selectors", () => {
+	it("selects a tag by id", () => {
+		expect(tagsSliceSelectors.selectTagById(
+			initialState,
+			initialId
+		)).toEqual(initialState.tags.tags[initialId]);
+	});
+});
