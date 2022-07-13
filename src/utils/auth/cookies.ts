13c2aac0fc687b59097d25f72984fbe0619a5f64
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-13 22:50:12 +0700

    Create `cookies` utils

--- src/utils/auth/cookies.ts
+++ src/utils/auth/cookies.ts
@@ -1,7 +1,17 @@
-import nookies from "nookies";
+import type { NextRequest } from "next/server";
 
-// export enum CookieKeys {
-//     TOKEN = "token"
-// }
+export enum CookieKeys {
+    TOKEN = "token"
+}
 
-// nookies.set(null, "", 1,);
+export interface CookieConfigs {
+	secure: true;
+	httpOnly: true;
+	sameSite: true;
+
+    /** Next week from now in seconds */
+	maxAge: 604800;
+	expires: Date;
+}
+
+export const getMiddlewareCookies = (req: NextRequest) => req.cookies;
